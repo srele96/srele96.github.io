c4b54a577d128ed2803ba9d85f983b662538180e
Srecko <srele96@gmail.com>
2020-11-19 13:11:08 +0100

    Added new card page and page descriptions
    Pages were confusing and vague, why are they there and what they
    broadcast
    Added headers, articles, links for further ease of navigation,
    explainations
    Changed styles to be more consistent from page to page

--- xd.js
+++ xd.js
@@ -1,46 +1,26 @@
 document.getElementsByClassName('container')[0]
-  .addEventListener('mouseenter', e => {
-    const teye = document.getElementsByClassName('t-eye')[0];
-    const deye = document.getElementsByClassName('d-eye')[0];
-
-    teye.classList.toggle('cross-t-eye');
-    deye.classList.toggle('cross-d-eye');
-
-    const bottomLip = document.getElementsByClassName('bottom-lip')[0];
-    const upperLip = document.getElementsByClassName('upper-lip')[0];
-    bottomLip.classList.toggle('big-smile');
-    upperLip.classList.toggle('close-big-smile');
-
-    const mouth = document.getElementsByClassName('mouth')[0];
-    mouth.classList.toggle('move-smile');
-
-    const textBubble = document.getElementsByClassName('bubble-text')[0];
-    textBubble.innerHTML = 'XDDDDDDDD';
-
-    const bubble = document.getElementsByClassName('bubble')[0];
-    bubble.classList.toggle('bubble-hovered');
-  });
+  .addEventListener('mouseenter', toggle);
 
 document.getElementsByClassName('container')[0]
-.addEventListener('mouseleave', e => {
+  .addEventListener('mouseleave', toggle);
+
+document.getElementsByClassName('container')[0]
+  .addEventListener('click', toggle);
+
+function toggle(e) {
   const teye = document.getElementsByClassName('t-eye')[0];
   const deye = document.getElementsByClassName('d-eye')[0];
+  const bottomLip = document.getElementsByClassName('bottom-lip')[0];
+  const upperLip = document.getElementsByClassName('upper-lip')[0];
+  const mouth = document.getElementsByClassName('mouth')[0];
+  const textBubble = document.getElementsByClassName('bubble-text')[0];
+  const bubble = document.getElementsByClassName('bubble')[0];
 
   teye.classList.toggle('cross-t-eye');
   deye.classList.toggle('cross-d-eye');
-
-  const bottomLip = document.getElementsByClassName('bottom-lip')[0];
-  const upperLip = document.getElementsByClassName('upper-lip')[0];
   bottomLip.classList.toggle('big-smile');
   upperLip.classList.toggle('close-big-smile');
-
-  const mouth = document.getElementsByClassName('mouth')[0];
   mouth.classList.toggle('move-smile');
-
-  const textBubble = document.getElementsByClassName('bubble-text')[0];
-  textBubble.innerHTML = 'Hey! Hover me.';
-
-  
-  const bubble = document.getElementsByClassName('bubble')[0];
+  textBubble.innerHTML = 'Hey! Hover me.';  
   bubble.classList.toggle('bubble-hovered');
-});
+}
